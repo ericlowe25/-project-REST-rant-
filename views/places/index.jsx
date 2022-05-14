6bc40b45052e6065a03757ea92a6773e0cb87cc8
Eric Lowe <ericlowe25@gmail.ocm>
2022-05-14 13:42:31 -0400

    issue PART3 step 6

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,18 +1,19 @@
 function index (data) {
     let placesFormatted = data.places.map((place) => {
-        return (
+      return (
         <div>
-           <h2>{place.name}</h2>
-           <img src={place.pic} alt={place.name}></img>
-        </div>)
+          <h2>{place.name}</h2>
+          <img src={place.pic} alt={place.name}>
+        </div>
+      )
     })
-
-return (
-    <Def>
-        <main>
-            <h1>PLACES INDEX PAGE</h1>
-            {placesFormatted}
-        </main>
-    </Def>
-)
-}
+    return (
+      <Def>
+          <main>
+              <h1>PLACES INDEX PAGE</h1>
+              {placesFormatted}
+          </main>
+      </Def>
+  )
+  }
+  
